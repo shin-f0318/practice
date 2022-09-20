dff56abab2c8fd54dde012746952c77b300bb697
shin-f0318 <sf.0318ss@gmail.com>
2022-09-20 23:09:24 +0900

    poractice5

---
+++ js/Homework2.js
@@ -0,0 +1,9 @@
+console.log("Hellow!");
+
+const userName = "My name is Shinya Fujimoto";
+console.log(userName);
+
+ const userAddress = "I live in Japan";
+console.log(userAddress);
+
+console.log("Thank you!");
