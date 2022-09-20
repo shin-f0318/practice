4baa80c515f8674da26175b1913980462b36c06f
shin-f0318 <sf.0318ss@gmail.com>
2022-09-20 22:25:22 +0900

    practice4

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// ここにjavascriptのコードを記述する
+alert("Hellow!");
+
+ const userName = "My name is Shinya Fujimoto";
+console.log(userName);
+
+ const userAddress = "I live in Japan";
+console.log(userAddress);
+
+alert("Thank you");
