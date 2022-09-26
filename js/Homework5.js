053b9ce7ba3ff9755c56a5e187de4f8f51cb5ab5
shin-f0318 <sf.0318ss@gmail.com>
2022-09-26 21:24:35 +0900

    practice13

--- js/Homework5.js
+++ js/Homework5.js
@@ -10,34 +10,34 @@ for (let i2 = 10; i2 > -1; i2 --) {
 
 // 問題1 1から10までの和を表示する
 let sum = 0;
-for( i3 = 1 ; i3<=10 ; ++i3 )
+for(let i3 = 1 ; i3<=10 ; ++i3 )
 {
 	sum = sum + i3;
 }
 console.log(sum)
 
 // 問題2 50から100までを表示する
-let num1 = 49;
+let num1 = 50;
 
-while (num1 <= 99) {
-    num1 ++;
+while (num1 <= 100) {
     console.log(num1);
+    num1 ++;
 }
 
 // 問題2 100から50までを表示する
-let num2 = 101;
+let num2 = 100;
 
-while (num2 >= 51) {
-    num2 --;
+while (num2 >= 50) {
     console.log(num2);
+    num2 --;
 }
 
 // 問題2 50から100までの和を表示する←見直し
-let sum1 = 50;
-let i4 = 1;
+let sum1 = 0;
+let i4 = 50;
 while (i4 <= 100) {
+    sum1 = sum1 + i4;
     i4 ++;
-    sum1= sum1 + i4;
     console.log(sum1);
 }
 
@@ -50,9 +50,24 @@ let total = numbers.reduce((sum2, element) => sum2 + element, 0);
 
 console.log(total);
 
+let total2 = 0;
+for(let i5 = 0;i5 < numbers.length; i5++){
+    total2 = total2 + numbers[i5];
+}
+console.log(total2);
+
 /* 問題4 以下の連想配列を準備します。
 const personalData1 = { name: '侍太郎', age: 36, gender: '男性', address: '東京都', phoneNumber: '070-0809-1160' };
 const personalData2 = { name: '侍花子', age: 20, gender: '女性', address: '京都府', phoneNumber: '999-9999-9999' };
 ・こちらの2つのデータから平均年齢を出すプログラムを作成してください。 */
 const personalData1 = { name: '侍太郎', age: 36, gender: '男性', address: '東京都', phoneNumber: '070-0809-1160' };
 const personalData2 = { name: '侍花子', age: 20, gender: '女性', address: '京都府', phoneNumber: '999-9999-9999' };
+
+console.log(personalData1['age']);
+console.log(personalData2['age']);
+console.log((personalData1['age'] + personalData2['age']) / 2);
+
+const addTwoArguments = (num1,num2) => {
+    return(num1+num2)/2;
+}
+console.log(addTwoArguments(personalData1['age'],personalData2['age']));
